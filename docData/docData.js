6a12e9b7e121daf218d6666a7558e19ea4c4289c
Dev Sharma <devsharma1683@gmail.com>
2025-02-09 00:11:49 +0530

    added appointment booking html and css file and created the data.JS file for doctors data

---
+++ docData/docData.js
@@ -0,0 +1,113 @@
+const doctorList = [
+  {
+    id: "01",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr Jaganmani Sreekanth",
+    yrCat: "25 years,Internal Medicine",
+    degree: "MBBS; Diploma (Ophthalmology); MD (Internal Medicine)",
+    language: "English,Hindi",
+    description:
+      "Dr Jaganmani Sreekanth is an experienced Interventional based in Hyderabad. Dr Jaganmani Sreekanth graduated from MBBS",
+      appntTime:"(12:00 - 13:15)"
+  },
+  
+  {
+    id: "02",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr S N Pathak",
+    yrCat: "17 years,Cardiologist",
+    degree: "MBBS, MD(Medicine), DM (Cardiology)",
+    language: "English,Hindi",
+    description:
+      "Dr S N Pathak is an experienced Interventional based in Delhi. Dr S N Pathak graduated from MBBS, MD(Medicine), DM (Cardiology)",
+      appntTime:"(10:00 - 15:00)"
+  },
+  {
+    id: "03",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr Avinash Gupta",
+    yrCat: "11 years,Neurologist",
+    degree: "MBBS,DNB(MED),DNB(NEUROLOGY)",
+    language: "English,Hindi",
+    description:
+      "Dr Avinash Gupta is an experienced Interventional based in Bilaspur. Dr Avinash Gupta graduated from MBBS,DNB(MED),DNB(NEUROLOGY)",
+      appntTime:"(10:00 - 16:00)"
+  },
+  {
+    id: "04",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr Amit Gharat",
+    yrCat: "13 years,Gastroenterologist",
+    degree: "MBBS, MD, DNB",
+    language: "English,Hindi",
+    description:
+      "Dr Amit Gharat is an experienced Interventional based in Mumbai. Dr Amit Gharat graduated from MBBS, MD, DNB",
+      appntTime:"(13:30 - 16:30)"
+  },
+  {
+    id: "05",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr S R K Dikshith",
+    yrCat: "12 years, Orthopedician",
+    degree: "MS .Ortho ( gold medalist) , DNB . Ortho, FIJR Fellow in Advanced Arthroscopy",
+    language: "English,Hindi",
+    description:
+      "Dr S R K Dikshith is an experienced Interventional based in Hyderabad. Dr S R K Dikshith graduated from MS .Ortho ( gold medalist) , DNB . Ortho, FIJR Fellow in Advanced Arthroscopy",
+      appntTime:"(10:30 - 15:30)"
+  },
+  {
+    id: "06",
+    image: "../assets/doc-female-new.png",
+    docName: "Dr S Madhuri",
+    yrCat: "8 years, Dermatologist",
+    degree: "MBBS, MD DVL, DNB DVL, FELLOW IN DERMATOSURGERY AND LASERS",
+    language: "English,Hindi",
+    description:
+      "Dr S Madhuri is an experienced Interventional based in Hyderabad. Dr S Madhuri graduated from MBBS, MD DVL, DNB DVL, FELLOW IN DERMATOSURGERY AND LASERS",
+      appntTime:"(14:00 - 16:00)"
+  },
+  {
+    id: "07",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr Rajib Paul",
+    yrCat: "20 years,Plastic Surgeon",
+    degree: "MBBS; MD (General Medicine)",
+    language: "English,Hindi",
+    description:
+      "Dr Rajib Paul is an experienced Interventional based in Hyderabad. Dr Rajib Paul graduated from MBBS; MD (General Medicine)",
+      appntTime:"(12:30 - 14:15)"
+  },
+  {
+    id: "08",
+    image: "../assets/doc-female-new.png",
+    docName: "Dr Sripriya Sanker",
+    yrCat: "26 years, Opthalmologist",
+    degree: "MBBS, Diploma in Ophthalmology (DO), FRCS (Glasg)",
+    language: "English,Hindi",
+    description:
+      "Dr Sripriya Sankar is an experienced Interventional based in Chennai. Dr Sripriya Sankar graduated from MBBS, Diploma in Ophthalmology (DO), FRCS (Glasg)",
+      appntTime:"(11:00 - 12:00)"
+  },
+  {
+    id: "09",
+    image: "../assets/doc-female-new.png",
+    docName: "Dr Rashmi Sudhir",
+    yrCat: "15 years, Radiologist",
+    degree: "MBBS(CMC,Vellore), DNB (Radiology)",
+    language: "English,Hindi",
+    description:
+      "Dr Rashmi Sudhir is an experienced Interventional based in Hyderabad. Dr Rashmi Sudhir graduated from MBBS(CMC,Vellore)",
+      appntTime:"(10:00 - 16:00)"
+  },
+  {
+    id: "10",
+    image: "../assets/doc-male-new.png",
+    docName: "Dr Jaisom Chopra",
+    yrCat: "35 years, Vascular Surgeon",
+    degree: "BSc, MBBS, MS, FRCS(Edin)",
+    language: "English,Hindi",
+    description:
+      "Dr Jaisom Chopra is an experienced Interventional based in Delhi. Dr Jaisom Chopra graduated from BSc, MBBS, MS, FRCS(Edin)",
+      appntTime:"(10:00 - 17:00)"
+  },
+];
